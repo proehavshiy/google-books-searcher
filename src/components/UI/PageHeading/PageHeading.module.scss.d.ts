601f0a22fd74261dbc401baa75cc77e973a742ad
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-09-05 17:07:17 +0300

    refactor: PageHeading with ts

---
+++ src/components/UI/PageHeading/PageHeading.module.scss.d.ts
@@ -0,0 +1,1 @@
+export const pageHeading: string;
--- src/components/UI/PageHeading/PageHeading.tsx
+++ src/components/UI/PageHeading/PageHeading.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './PageHeading.module.scss';
 const cn = classNames.bind(styles);
 
-function PageHeading({ children }) {
+interface IPageHeading {
+  children: React.ReactNode;
+}
+
+const PageHeading: FC<IPageHeading> = ({ children }) => {
   return (
     <h1 className={cn('page-heading')}>
       {children}
     </h1>
   );
-}
+};
 
 export default PageHeading;
